3cd552b86f01bfc35bd18c9ab9c298327ca967b8
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-07-15 22:03:14 -0500

    Primer commit

---
+++ Herencia/src/Animal.java
@@ -0,0 +1,15 @@
+
+public class Animal {
+
+	String nombre;
+	int patas;
+	int edad;
+	
+	public String comer() {
+		return "Comiendo";
+	}	
+	public String dormir() {
+		return "Durmiendo";
+
+	}
+}
---
+++ Herencia/src/Gato.java
@@ -0,0 +1,8 @@
+
+public class Gato  extends Animal{
+	
+	int num_bogotes;
+	String color;
+	int vidas_disponibles = 9;
+
+}
